a533a4cc046302aaf5a8238e3e87ebbbfd47d46c
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-07 11:39:50 +0100

    Prueba definitiva (vuelvo a lo anterior pero con comentarios)

--- propuesta.cpp
+++ propuesta.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
 using namespace std;
 
 int main() {
-    vector<string> resultados;
-
-    for (int a = 1; a <= 20; a++) {
-        for (int b = a; b <= 20; b++) {
-            int c = sqrt(a * a + b * b);
-            if (a * a + b * b == c * c && a + b > c) {
-                if ((a == 3 && b == 4 && c == 5) ||
+    for (int a = 1; a <= 20; a++) { // empezamos en 1 porque no hay triángulo con lados 0 y creamos un contador para a
+        for (int b = a; b <= 20; b++) { // empezamos en a porque no hay triángulo con lados menores que a y creamos un contador para b
+            int c = sqrt(a* a + b * b); // calculamos c
+            if (a * a + b * b == c*c && a + b > c) { // comprobamos si es un triángulo rectángulo
+                if ((a == 3 && b == 4 && c == 5) || // seleccionamos según los valores que nos piden
                     (a == 6 && b == 8 && c == 10) ||
                     (a == 5 && b == 12 && c == 13) ||
                     (a == 8 && b == 15 && c == 17)) {
-                    resultados.push_back("- " + to_string(a) + " - " + to_string(b) + " - " + to_string(c));
+                    cout << "- " << a << " - " << b << " - " << c << endl; // imprimimos el triángulo
                 }
             }
         }
-    }
-    for (const string& resultado : resultados) {
-        cout << resultado << endl;
     }
     return 0;
 }
