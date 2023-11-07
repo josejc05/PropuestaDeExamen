e2debfbc7b632d82ec08dcb4d204488cb1e3132b
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-07 11:12:04 +0100

    Prueba 4

--- propuesta.cpp
+++ propuesta.cpp
@@ -2,11 +2,11 @@
 #include <cmath>
 using namespace std;
 int main() {
-    for (int a = 1; a <= 20; a++) {
-        for (int b = a; b <= 20; b++) {
+    for (int a = 1; a <= 12; a++) {
+        for (int b = a; b <= 12; b++) {
             int c = sqrt(a * a + b * b);
             if (a * a + b * b == c * c && a + b > c) {
-                cout << a << " - " << b << " - " << c;
+                cout << a << " - " << b << " - " << c<< endl;
             }
         }
     }
