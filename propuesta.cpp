c2eb169cb97c2ebdb2f56c60e5be37d12855e091
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-07 11:13:42 +0100

    Prueba 5

--- propuesta.cpp
+++ propuesta.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
+
 int main() {
-    for (int a = 1; a <= 12; a++) {
-        for (int b = a; b <= 12; b++) {
-            int c = sqrt(a * a + b * b);
-            if (a * a + b * b == c * c && a + b > c) {
-                cout << a << " - " << b << " - " << c<< endl;
+    int a, b, c;
+    for (a = 1; a <= 20; a++) {
+        for (b = a; b <= 20; b++) {
+            for (c = b; c <= 20; c++) {
+                if (a * a + b * b == c * c && (a * a + b * b <= 20 * 20) && (a + b + c <= 30)) {
+                    if ((a == 3 && b == 4 && c == 5) ||
+                        (a == 6 && b == 8 && c == 10) ||
+                        (a == 5 && b == 12 && c == 13) ||
+                        (a == 8 && b == 15 && c == 17)) {
+                        cout << "- " << a << " - " << b << " - " << c << endl;
+                    }
+                }
             }
         }
     }
     return 0;
 }
+
